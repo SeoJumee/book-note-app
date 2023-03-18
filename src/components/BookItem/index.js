91717acf9e4c89798edef6e66d7bd68904f2f06b
서주미 <official.jumee@gmail.com>
2023-03-18 22:48:44 +0900

    Add BookItem component

---
+++ src/components/BookItem/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as S from './style';
+
+export default function BookItem() {
+  return (
+    <>
+      <S.BookItem />
+      <S.BookItem />
+      <S.BookItem />
+      <S.BookItem />
+      <S.BookItem />
+      <S.BookItem />
+      <S.BookItem />
+      <S.BookItem />
+    </>
+  );
+}
---
+++ src/components/BookItem/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const BookItem = styled.div`
+  width: 380px;
+  height: 150px;
+  background-color: #d9d9d9;
+`;
