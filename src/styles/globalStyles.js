ffc024c57f84ca91f9e063ea5efdac9d56ef9912
서주미 <official.jumee@gmail.com>
2023-03-18 22:44:14 +0900

    폰트 적용

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,7 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
+import GaeguRegular from '../assets/font/Gaegu-Regular.ttf';
 
 export const GlobalStyles = createGlobalStyle`
+@font-face {
+    font-family: 'Gaegu';
+    src: local('GaeguRegular'), local('GaeguRegular');
+    src: url(${GaeguRegular}) format('truetype');
+}
 * {
+    font-family: 'Gaegu';
     padding: 0;
     margin: 0;
     box-sizing: border-box;
